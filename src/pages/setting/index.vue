655ae121dbb05028fc9922b9d806bfd342539497
orozot <renzhitong12@outlook.com>
2023-04-11 19:58:42 +0800

    feat: add gitlab settings

--- src/pages/setting/index.vue
+++ src/pages/setting/index.vue
@@ -1,24 +1,12 @@
 <script setup>
-  import { reactive } from 'vue';
-
-  const settingsForm = reactive({
-    accessToken: '',
-  });
+  import Integration from '@/pages/setting/integration-setting/index.vue';
 </script>
 
 <template>
-  <div class="setting tw-flex tw-justify-center tw-m-10 tw-colors-text-6">
-    <a-form
-      :model="settingsForm"
-      :style="{ width: '600px' }"
-      @submit="handleSubmit"
-    >
-      <a-form-item field="accessToken" label="Access Token">
-        <a-input
-          v-model="settingsForm.accessToken"
-          placeholder="please enter your access token"
-        />
-      </a-form-item>
-    </a-form>
+  <div class="setting tw-flex tw-justify-center main-content">
+    <a-list class="tw-w-full tw-max-w-2xl" :bordered="false">
+      <template #header> Settings </template>
+      <integration />
+    </a-list>
   </div>
 </template>
---
+++ src/pages/setting/integration-setting/edit-modal.vue
@@ -0,0 +1,41 @@
+<script setup>
+  import { reactive, ref } from 'vue';
+
+  const visible = ref(true);
+  const gitlabForm = reactive({
+    token: '',
+    url: '',
+  });
+
+  const emits = defineEmits(['close']);
+
+  const handleOk = () => {
+    // todo
+  };
+
+  const handleClose = () => {
+    emits('close');
+  };
+</script>
+
+<template>
+  <a-modal
+    v-model:visible="visible"
+    :simple="true"
+    @ok="handleOk"
+    @before-close="handleClose"
+  >
+    <template #title> Set up GitLab </template>
+    <div>
+      <a-form :model="gitlabForm" layout="vertical">
+        <a-form-item field="url" label="GitLab hostname">
+          <a-input v-model="gitlabForm.url" />
+        </a-form-item>
+        <a-form-item field="token" label="API Access Token">
+          <a-input v-model="gitlabForm.token"> </a-input>
+          <a-button type="primary">Test</a-button>
+        </a-form-item>
+      </a-form>
+    </div>
+  </a-modal>
+</template>
